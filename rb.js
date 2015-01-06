38cf6c174e7ff71458782ee8c2921f6ee2f66a61
Pierce McEntagart <pierce.mt92@gmail.com>
2015-01-06 13:07:45 -0800

    Added some boilerplate for red-black trees.

---
+++ rb.js
@@ -0,0 +1,11 @@
+function RedBlackTree() {
+    this.parent = null;
+    this.leftChild = null;
+    this.rightChild = null;
+}
+
+RedBlackTree.prototype.insert = function(value) {
+}
+
+RedBlackTree.prototype.del = function(value) {
+}
