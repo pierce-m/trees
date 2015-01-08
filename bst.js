80110613c23f8b307eb3c1d6fe759ae57a36d3b6
Pierce McEntagart <pierce.mt92@gmail.com>
2015-01-07 18:46:15 -0800

    Moved bst to use 2 space indentation instead of 4.

--- bst.js
+++ bst.js
@@ -1,34 +1,29 @@
 function BST() {
-    this.root = null;
-    this.isEmpty = true;
+  this.root = null;
+  this.isEmpty = true;
 }
 
 BST.prototype.insert = function(key) {
-    z = new BSTNode(key, null);
-    if (this.isEmpty) {
-        this.isEmpty = false;
-        this.root = z;
-    }
-    var y = null;
-    var x = this.root;
-    while (x != null) {
-        y = x;
-        if (z.key < x.key) {
-            x = x.leftChild;
-        } else {
-            x = x.rightChild;
-        }
-    }
-    z.parent = y;
-    if (y == null) {
-        this.root = z;
-    } else if (z.key < y.key) {
-        y.setLeftChild(z);
+  var z = new BSTNode(key, null);
+  var y = null;
+  var x = this.root;
+  while (x != null) {
+    y = x;
+    if (z.key < x.key) {
+      x = x.leftChild;
     } else {
-        y.setRightChild(z);
+      x = x.rightChild;
     }
-    //this.rebalance(z);
-    return [{action:"ADD-CHILD", nodes:z.vis}];
+  }
+  z.parent = y;
+  if (y == null) {
+    this.root = z;
+  } else if (z.key < y.key) {
+    y.setLeftChild(z);
+  } else {
+    y.setRightChild(z);
+  }
+  return [{action:"ADD-CHILD", child:z}];
 }
 
 BST.prototype.del = function(key) {
@@ -36,28 +31,27 @@ BST.prototype.del = function(key) {
 
 BST.prototype.type = "bst";
 
-function BSTNode(key, par, cx, cy) {
-    this.key = key;
-    this.parent = par;
-    this.cx = cx;
-    this.cy = cy;
-    this.leftChild = null;
-    this.rightChild = null;
-// the visual representation of this node
-    this.vis = {"name":key, "parent": par, "children": []}; 
+function BSTNode(key, par) {
+  this.key = key;
+  this.parent = par;
+  this.cx = 0;
+  this.cy = 0;
+  this.leftChild = null;
+  this.rightChild = null;
 }
 
 BSTNode.prototype.setLeftChild = function (child) {
-    this.leftChild = child;
-    this.vis["children"].push(child);
+  this.leftChild = child;
+  child.cx = this.cx - 20;
+  child.cy = this.cy + 20;
 }
 
 BSTNode.prototype.setRightChild = function (child) {
-    this.RightChild = child;
-    this.vis["children"].push(child);
+  this.RightChild = child;
+  child.cx = this.cx + 20;
+  child.cy = this.cy + 20;
 }
 
 BSTNode.prototype.setParent = function (parent) {
-    this.parent = parent;
-    this.vis["parent"] = parent.key;
+  this.parent = parent;
 }
