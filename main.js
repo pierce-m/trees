952c382fae7f465f478c40d66941070b825f15fb
Pierce McEntagart <pierce.mt92@gmail.com>
2015-01-07 18:44:18 -0800

    Trimmed main while I figure out the best way to approach this with generality.

--- main.js
+++ main.js
@@ -8,88 +8,64 @@
  */
 function makeTree(type, w, h, canvas) {
 
-    /* Underlying d3 layout tree */
-    var vistree = d3.layout.tree();
-    /* svg on which to append tree nodes */
-    var svg = d3.select(canvas)
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h);
+  /* Underlying d3 layout tree */
+  var vistree = d3.layout.tree();
+  /* svg on which to append tree nodes */
+  var svg = d3.select(canvas)
+              .append("svg")
+              .attr("width", w)
+              .attr("height", h);
 
-    /* Runs a series of updates on the existing visualization based on
-     * the commands supplied in ops.
-     * @param ops, a list of operations 
-     */
-    function update(ops) {
+  /* Runs a series of updates on the existing visualization 
+   * based on the commands supplied in ops.
+   * @param ops, a list of operations 
+   */
+  function update(ops) {
 
-        function RedBlackOps(type, ops) {
-            for (var i in ops) {
-                switch (ops[i].action) {
-                    case "RECOLOR":
-                        // switch color of included nodes
-                        break;
-                    case "LEFT-ROTATE":
-                        // left rotate around node
-                        break;
-                    case "RIGHT-ROTATE":
-                        // right rotate around node
-                        break;
-                    case "ADD-CHILD":
-                        // adds a child to a node
-                        break;
-                }
-            }
+    function RedBlackOps(type, ops) {
+    }
+  
+    function BSTOps(ops) {
+      for (var i in ops) {
+        switch (ops[i].action) {
+          case "ADD-CHILD":
+            break;
         }
-    
-        function BSTOps(ops) {
-            for (var i in ops) {
-                switch (ops[i].action) {
-                    case "ADD-CHILD":
-                        svg.selectAll("circle")
-                           .data(vistree.nodes(ops[i].nodes))
-                           .enter().append("circle")
-                           .attr("class", "rb-node")
-                           .attr("cx", function (d) { return d.x + w/2; })
-                           .attr("cy", function (d) { return d.y + 20; })
-                           .attr("r", 15)
-                           .style("stroke", function(d) { return "black"; });
-                        break;
-                }
-            }
-        }
-
-        switch (type) {
-            case "bst":
-                BSTOps(ops);
-                break;
-            case "rb":
-                RedBlackOps(ops);
-                break;
-        }
+      }
     }
 
-    /* The underlying tree datastructure */
-    function tree(type) {
-        this.type = type;
-        switch (type) {
-            case "bst":
-                this.tree = new BST ();
-                break;
-            case "rb":
-                this.tree = new RedBlackTree ();
-                break;
-            default:
-                throw "Invalid tree type: " + type;
-        }
+    switch (type) {
+      case "bst":
+        BSTOps(ops);
+        break;
+      case "rb":
+        RedBlackOps(ops);
+        break;
     }
+  }
 
-    tree.prototype.insert = function (val) {
-        update(this.tree.insert(val));
+  /* The underlying tree datastructure */
+  function tree(type) {
+    this.type = type;
+    switch (type) {
+      case "bst":
+        this.tree = new BST ();
+        break;
+      case "rb":
+        this.tree = new RedBlackTree ();
+        break;
+      default:
+        throw "Invalid tree type: " + type;
     }
+  }
 
-    tree.prototype.del = function (val) {
-        update(this.tree.del(val));
-    }
+  tree.prototype.insert = function (val) {
+    update(this.tree.insert(val));
+  }
 
-    return new tree(type);
+  tree.prototype.del = function (val) {
+    update(this.tree.del(val));
+  }
+
+  return new tree(type);
 }
