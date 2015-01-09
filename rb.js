a1c5e366a800159995d60292395d4b97d3ca7e66
Pierce McEntagart <pierce.mt92@gmail.com>
2015-01-08 16:28:03 -0800

    Retrofitted rb.js to conform to new visualization interface, some minor fixes.

--- rb.js
+++ rb.js
@@ -1,18 +1,11 @@
 function RedBlackTree(selection) {
     this.root = null;
-    this.isEmpty = true;
 }
 
 RedBlackTree.prototype.insert = function(key) {
-    if (this.isEmpty) {
-        this.isEmpty = false;
-        z = new RedBlackTreeNode(key, null, w / 2, 20);
-    } else {
-        z = new RedBlackTreeNode(key);
-    }
-    var ops = []
+    var z = new RedBlackTreeNode(key);
     var y = null;
-    x = this.root;
+    var x = this.root;
     while (x != null) {
         y = x;
         if (z.key < x.key) {
@@ -29,98 +22,72 @@ RedBlackTree.prototype.insert = function(key) {
     } else {
         y.setRightChild(z);
     }
-    //this.rebalance(z);
-    return [{action:"ADD-CHILD", nodes:z.vis}];
 }
 
 RedBlackTree.prototype.del = function(key) {
 }
 
-//RedBlackTree.prototype.leftRotate = function (node) {
-//    var y = node.rightChild;
-//    node.setRightChild(y.leftChild);
-//    if (y.leftChild != null) {
-//        y.leftChild.parent = node;
-//    }
-//    y.parent = node.parent;
-//    if (node.parent == null) {
-//        this.root = y;
-//    } else if (node == node.parent.leftChild) {
-//        node.parent.setLeftChild(y);
-//    } else {
-//        node.parent.setRightChild(y);
-//    }
-//    y.setLeftChild(node);
-//    node.parent = y;
-//}
-//
-//RedBlackTree.prototype.rightRotate = function (node) {
-//    var y = node.setLeftChild;
-//    node.setLeftChild(y.rightChild);
-//    if (y.rightChild != null) {
-//        y.rightChild.parent = node;
-//    }
-//    y.parent = node.parent;
-//    if (node.parent == null) {
-//        this.root = y;
-//    } else if (node == node.parent.leftChild) {
-//        node.parent.setLeftChild(y);
-//    } else {
-//        node.parent.setRightChild(y);
-//    }
-//    y.setRightChild = node;
-//    node.parent = y;
-//}
+RedBlackTree.prototype.leftRotate = function (node) {
+    var y = node.rightChild;
+    node.setRightChild(y.leftChild);
+    if (y.leftChild != null) {
+        y.leftChild.setParent(node);
+    }
+    y.setParent(node.parent);
+    if (node.parent == null) {
+        this.root = y;
+    } else if (node == node.parent.leftChild) {
+        node.parent.setLeftChild(y);
+    } else {
+        node.parent.setRightChild(y);
+    }
+    y.setLeftChild(node);
+    node.setParent(y);
+}
+
+RedBlackTree.prototype.rightRotate = function (node) {
+    var y = node.setLeftChild;
+    node.setLeftChild(y.rightChild);
+    if (y.rightChild != null) {
+        y.rightChild.setParent(node);
+    }
+    y.parent = node.parent;
+    if (node.parent == null) {
+        this.root = y;
+    } else if (node == node.parent.leftChild) {
+        node.parent.setLeftChild(y);
+    } else {
+        node.parent.setRightChild(y);
+    }
+    y.setRightChild = node;
+    node.setParent(y);
+}
 
 RedBlackTree.prototype.rebalance = function(node) {
-//    while (node.parent.color == "red") {
-//        if (z.parent == z.parent.parent.leftChild) {
-//            var y = z.parent.parent.rightChild;
-//            if (y.color == "red") {
-//                z.parent.color = "black";
-//                y.color = "black";
-//                z.parent.parent.color = "red";
-//                z = z.parent.parent;
-//            } else {
-//                if (z == z.parent.rightChild) {
-//                    z = z.parent;
-//                    this.leftRotate(z);
-//                }
-//
-//            }
-//        }
-//    }
 }
 
 RedBlackTree.prototype.type = "rb";
 
-function RedBlackTreeNode(key, par, cx, cy) {
+function RedBlackTreeNode(key) {
     this.key = key;
+    this.depth = null;
+    this.parent = null;
     this.color = "red";
-    this.parent = par;
-    this.cx = cx;
-    this.cy = cy;
     this.leftChild = null;
     this.rightChild = null;
-// the visual representation of this node
-    this.vis = {"name":key, "parent": par, "children": []}; 
+    this.isLeftChild = false;
 }
 
 RedBlackTreeNode.prototype.setLeftChild = function (child) {
     this.leftChild = child;
-    this.leftChild.cx = this.cx - 30;
-    this.leftChild.cy = this.cy + 30;
-    this.vis["children"].push(child);
+    child.isLeftChild = true;
 }
 
 RedBlackTreeNode.prototype.setRightChild = function (child) {
     this.RightChild = child;
-    this.leftChild.cx = this.cx + 30;
-    this.leftChild.cy = this.cy + 30;
-    this.vis["children"].push(child);
 }
 
 RedBlackTreeNode.prototype.setParent = function (parent) {
-    this.parent = parent;
-    this.vis["parent"] = parent;
+  this.parent = parent;
+  this.depth = parent ? parent.depth + 1: 0;
 }
